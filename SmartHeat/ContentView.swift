f381ee9aa11177eee8cd40ec6efbeab0cc68b08c
mariusschaffrath <gqzz6xfzxd@privaterelay.appleid.com>
2026-03-20 09:11:12 +0100

    Initial Commit

---
+++ SmartHeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartHeat
+//
+//  Created by Marius Schaffrath on 20.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartHeat/SmartHeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHeatApp.swift
+//  SmartHeat
+//
+//  Created by Marius Schaffrath on 20.03.26.
+//
+
+import SwiftUI
+
+@main
+struct SmartHeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
